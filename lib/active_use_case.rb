83cf31c7e3859e83af2a1a75f7dddc427a54c510
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2016-05-13 07:51:55 +0200

    Remove useless autoload

--- lib/active_use_case.rb
+++ lib/active_use_case.rb
@@ -1,13 +1,11 @@
 require 'active_use_case/engine'
 require 'active_use_case/base'
 require 'active_use_case/errors'
+require 'active_use_case/model'
 require 'active_use_case/use_case'
 require 'active_use_case/version'
 
 module ActiveUseCase
-  autoload :Model,         'active_use_case/model'
-  autoload :ModelBehavior, 'active_use_case/model_behavior'
-
   class << self
 
     def logger=(logger)
