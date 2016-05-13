95aa5de166ce4f2cef3482c8bb3474cf9e8b8a90
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2016-05-13 23:09:37 +0200

    Update DB schema of dummy app

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -13,55 +13,8 @@
 
 ActiveRecord::Schema.define(version: 20131030014902) do
 
-  create_table "audits", force: true do |t|
-    t.integer  "auditable_id"
-    t.string   "auditable_type"
-    t.integer  "associated_id"
-    t.string   "associated_type"
-    t.integer  "user_id"
-    t.string   "user_type"
-    t.string   "username"
-    t.string   "action"
-    t.text     "audited_changes"
-    t.integer  "version",         default: 0
-    t.string   "comment"
-    t.string   "remote_address"
-    t.string   "request_uuid"
-    t.datetime "created_at"
-  end
-
-  add_index "audits", ["associated_id", "associated_type"], name: "associated_index"
-  add_index "audits", ["auditable_id", "auditable_type"], name: "auditable_index"
-  add_index "audits", ["created_at"], name: "index_audits_on_created_at"
-  add_index "audits", ["request_uuid"], name: "index_audits_on_request_uuid"
-  add_index "audits", ["user_id", "user_type"], name: "user_index"
-
-  create_table "comments", force: true do |t|
+  create_table "comments", force: :cascade do |t|
     t.text     "content"
-    t.integer  "user_id"
-    t.integer  "recipe_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "comments", ["recipe_id"], name: "index_comments_on_recipe_id"
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
-
-  create_table "recipes", force: true do |t|
-    t.string   "name"
-    t.text     "description"
-    t.string   "image_url"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "recipes", ["user_id"], name: "index_recipes_on_user_id"
-
-  create_table "users", force: true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
