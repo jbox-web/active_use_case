8329c0947a1e12c7161299daf89cf233ab44d867
Nicolas Rodriguez <nicoladmin@free.fr>
2019-02-18 23:14:53 +0100

    Use Zeitwerk to load gem files

--- lib/active_use_case.rb
+++ lib/active_use_case.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
-require 'active_use_case/engine'
-require 'active_use_case/base'
-require 'active_use_case/errors'
-require 'active_use_case/model'
-require 'active_use_case/use_case_builder'
-require 'active_use_case/version'
+require 'zeitwerk'
+Zeitwerk::Loader.for_gem.setup
 
 module ActiveUseCase
   mattr_accessor :logger
