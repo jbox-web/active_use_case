40916a40a6ed7db4703e8662aa3648fb03e8ff77
Nicolas Rodriguez <nicoladmin@free.fr>
2019-03-13 00:41:41 +0100

    Minor change

--- lib/active_use_case.rb
+++ lib/active_use_case.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 require 'zeitwerk'
-Zeitwerk::Loader.for_gem.setup
+loader = Zeitwerk::Loader.for_gem
+loader.setup
 
 module ActiveUseCase
   mattr_accessor :logger
