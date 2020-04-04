c124ee96145196ffc073ac8cd83e286e17e4a20c
Nicolas Rodriguez <nicoladmin@free.fr>
2020-04-04 17:28:19 +0200

    Bump to version 1.3.0

--- lib/active_use_case/version.rb
+++ lib/active_use_case/version.rb
@@ -8,8 +8,8 @@ module ActiveUseCase
 
   module VERSION
     MAJOR = 1
-    MINOR = 2
-    TINY  = 1
+    MINOR = 3
+    TINY  = 0
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
